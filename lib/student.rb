2ea4c65569ffe0c95428df3bc22ecd54839aadf5
Sara A <calisaraalhindi@gmail.com>
2020-09-14 20:36:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    binding.pry
+    # binding.pry
+    students_array.each{|student_details| Student.new(student_details)}
   end
 
   def add_student_attributes(attributes_hash)
