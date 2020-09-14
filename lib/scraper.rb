8bdd4f411c1775053a2c4918be26959219e9eec1
Sara A <calisaraalhindi@gmail.com>
2020-09-14 20:51:06 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,46 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+  
+    doc =Nokogiri::HTML(html)
     
+    doc.css(".student-card a").collect do |student|
+      {:name => student.css("h4").text,
+      :location => student.css("p").text,
+      :profile_url => student.attr("href")
+      }
+    end
   end
 
+  
+
   def self.scrape_profile_page(profile_url)
+    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    # binding.pry
+    profile_hash = {}
+# binding.pry
+    doc.css(".vitals-container .social-icon-container a").each do |detail| 
+        if detail.attr('href').include?("twitter")
+          profile_hash[:twitter] = detail.attr('href')
+        elsif detail.attr('href').include?("linkedin")
+          profile_hash[:linkedin] = detail.attr('href')
+        elsif detail.attr('href').include?("github")
+          profile_hash[:github] = detail.attr('href')
+        elsif detail.attr('href').include?("com/")
+          profile_hash[:blog] = detail.attr('href')
+        end
+        profile_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
+        profile_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
+      end
+    profile_hash
+    
     
   end
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,29 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name=student_hash[:name]
+    @location=student_hash[:location]
+    @profile_url=student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+   
+    students_array.each{|student_details| Student.new(student_details)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+      self.send("bio=", attributes_hash[:bio])
+      self.send("blog=", attributes_hash[:blog])
+      self.send("linkedin=", attributes_hash[:linkedin])
+      self.send("profile_quote=", attributes_hash[:profile_quote])
+      self.send("twitter=", attributes_hash[:twitter])
+  end
+  
+  def self.all
+    @@all
   end
 
-  def self.all
-    
-  end
+  
 end
 
